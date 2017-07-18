87261c3cad6cc30f0e8a7a69a4e3c0fda3c7752e
chesterlbtan <chesterlbtan@gmail.com>
2017-07-18 21:38:01 +0800

    Enable user to specfiy image format

--- BasicGrapher/Program.cs
+++ BasicGrapher/Program.cs
@@ -14,12 +14,35 @@ namespace BasicGrapher
         {
             //should specify the following
             // 1) json file with all the information
-            // 2) file name without extension
+            // 2) full path file name
             try
             {
                 ChartJSON objChartJson = new ChartJSON(File.ReadAllText(args[0]));
                 Chart chrtBarGraph = objChartJson.Chart;
-                chrtBarGraph.SaveImage(args[1] + ".png", ChartImageFormat.Png);
+
+                ChartImageFormat myFormat;
+                string[] extension = args[1].Split('.');
+                switch (extension[extension.Length - 1].ToLower())
+                {
+                    case "bmp":
+                        myFormat = ChartImageFormat.Bmp;
+                        break;
+                    case "tiff":
+                        myFormat = ChartImageFormat.Tiff;
+                        break;
+                    case "png":
+                        myFormat = ChartImageFormat.Png;
+                        break;
+                    case "gif":
+                        myFormat = ChartImageFormat.Gif;
+                        break;
+                    case "jpg":
+                    case "jpeg":
+                    default:
+                        myFormat = ChartImageFormat.Jpeg;
+                        break;
+                }
+                chrtBarGraph.SaveImage(args[1], myFormat);
             }
             catch (Exception ex)
             {
