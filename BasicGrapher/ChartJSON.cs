c98c8b8ebcebab2bff6454bf23a62b2cc5dceb9e
chesterlbtan <chesterlbtan@gmail.com>
2017-07-20 23:08:05 +0800

    Fix GetColor method Use Extension <With> to shorten lines.

--- BasicGrapher/ChartJSON.cs
+++ BasicGrapher/ChartJSON.cs
@@ -7,6 +7,13 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace BasicGrapher
 {
+    static class Extension
+    {
+        public static void With<T>(this T obj, Action<T> a)
+        {
+            a(obj);
+        }
+    }
     class ChartJSON : MyJSON
     {
         private Chart __chart;
@@ -26,66 +33,80 @@ namespace BasicGrapher
         private Chart ConvertToChart()
         {
             Chart dummy = new Chart();
-            foreach (var json_title in (List<JsonStruct>)_value["titles"].Value)
-            {
-                Title tmpTitle = new Title();
-                tmpTitle.Text = (string)json_title["text"].Value;
-                if (json_title.ContainsKey("docking"))
-                    tmpTitle.Docking = (Docking)Enum.Parse(typeof(Docking), (string)json_title["docking"].Value);
-                if (json_title.ContainsKey("font"))
-                    tmpTitle.Font = GetFont(json_title["font"]);
-                if (json_title.ContainsKey("forecolor"))
-                    tmpTitle.ForeColor = GetColor((string)json_title["forecolor"].Value);
-                dummy.Titles.Add(tmpTitle);
-            }
 
-            dummy.Size = new System.Drawing.Size((int)_value["size"]["width"].Value, (int)_value["size"]["height"].Value);
-            foreach (var json_chrtArea in (List<JsonStruct>)_value["chartareas"].Value)
+            dummy.With(a =>
             {
-                ChartArea tmpChrtArea = new ChartArea();
-                tmpChrtArea.AxisX.Title = (string)json_chrtArea["axisx"]["title"].Value;
-                if (json_chrtArea["axisx"].ContainsKey("font"))
-                    tmpChrtArea.AxisX.TitleFont = GetFont(json_chrtArea["axisx"]["font"]);
-                if (json_chrtArea["axisx"].ContainsKey("forecolor"))
-                    tmpChrtArea.AxisX.TitleForeColor = GetColor((string)json_chrtArea["axisx"]["forecolor"].Value);
-                if (json_chrtArea["axisx"].ContainsKey("minimum"))
-                    tmpChrtArea.AxisX.Minimum = (int)json_chrtArea["axisx"]["minimum"].Value;
-                if (json_chrtArea["axisx"].ContainsKey("maximum"))
-                    tmpChrtArea.AxisX.Maximum = (int)json_chrtArea["axisx"]["maximum"].Value;
-                if (json_chrtArea["axisx"].ContainsKey("interval"))
-                    tmpChrtArea.AxisX.Interval = (int)json_chrtArea["axisx"]["interval"].Value;
-
-                tmpChrtArea.AxisY.Title = (string)json_chrtArea["axisy"]["title"].Value;
-                if (json_chrtArea["axisy"].ContainsKey("font"))
-                    tmpChrtArea.AxisY.TitleFont = GetFont(json_chrtArea["axisy"]["font"]);
-                if (json_chrtArea["axisy"].ContainsKey("forecolor"))
-                    tmpChrtArea.AxisY.TitleForeColor = GetColor((string)json_chrtArea["axisy"]["forecolor"].Value);
-                if (json_chrtArea["axisy"].ContainsKey("minimum"))
-                    tmpChrtArea.AxisY.Minimum = (int)json_chrtArea["axisy"]["minimum"].Value;
-                if (json_chrtArea["axisy"].ContainsKey("maximum"))
-                    tmpChrtArea.AxisY.Maximum = (int)json_chrtArea["axisy"]["maximum"].Value;
-                if (json_chrtArea["axisy"].ContainsKey("interval"))
-                    tmpChrtArea.AxisY.Interval = (int)json_chrtArea["axisy"]["interval"].Value;
-
-                dummy.ChartAreas.Add(tmpChrtArea);
-            }
-            foreach (var json_series in (List<JsonStruct>)_value["series"].Value)
-            {
-                Series tmpSeries = new Series();
-                foreach (var json_points in (List<JsonStruct>)json_series["points"].Value)
+                foreach (var json_title in (List<JsonStruct>)_value["titles"].Value)
                 {
-                    DataPoint tmpPoint = new DataPoint();
-                    tmpPoint.AxisLabel = (string)json_points["axislabel"].Value;
-                    List<double> tmpYvalues = new List<double>();
-                    foreach (var json_Yvalue in (List<JsonStruct>)json_points["yvalues"].Value)
-                        tmpYvalues.Add((int)json_Yvalue.Value);
-                    tmpPoint.YValues = tmpYvalues.ToArray();
-                    tmpSeries.Points.Add(tmpPoint);
+                    Title tmpTitle = new Title();
+                    tmpTitle.Text = (string)json_title["text"].Value;
+                    if (json_title.ContainsKey("docking"))
+                        tmpTitle.Docking = (Docking)Enum.Parse(typeof(Docking), (string)json_title["docking"].Value);
+                    if (json_title.ContainsKey("font"))
+                        tmpTitle.Font = GetFont(json_title["font"]);
+                    if (json_title.ContainsKey("forecolor"))
+                        tmpTitle.ForeColor = GetColor((string)json_title["forecolor"].Value);
+                    a.Titles.Add(tmpTitle);
                 }
-                tmpSeries.XValueType = (ChartValueType)Enum.Parse(typeof(ChartValueType), (string)json_series["xvaluetype"].Value);
-                dummy.Series.Add(tmpSeries);
-            }
-            
+
+                a.Size = new System.Drawing.Size((int)_value["size"]["width"].Value, (int)_value["size"]["height"].Value);
+
+                foreach (var json_chrtArea in (List<JsonStruct>)_value["chartareas"].Value)
+                {
+                    ChartArea tmpChrtArea = new ChartArea();
+
+                    tmpChrtArea.AxisX.With(x =>
+                    {
+                        var axisX = json_chrtArea["axisx"];
+                        x.Title = (string)axisX["title"].Value;
+                        if (axisX.ContainsKey("font"))
+                            x.TitleFont = GetFont(axisX["font"]);
+                        if (axisX.ContainsKey("forecolor"))
+                            x.TitleForeColor = GetColor((string)axisX["forecolor"].Value);
+                        if (axisX.ContainsKey("minimum"))
+                            x.Minimum = (int)axisX["minimum"].Value;
+                        if (axisX.ContainsKey("maximum"))
+                            x.Maximum = (int)axisX["maximum"].Value;
+                        if (axisX.ContainsKey("interval"))
+                            x.Interval = (int)axisX["interval"].Value;
+                    });
+
+                    tmpChrtArea.AxisY.With(y =>
+                    {
+                        var axisY = json_chrtArea["axisy"];
+                        y.Title = (string)axisY["title"].Value;
+                        if (axisY.ContainsKey("font"))
+                            y.TitleFont = GetFont(axisY["font"]);
+                        if (axisY.ContainsKey("forecolor"))
+                            y.TitleForeColor = GetColor((string)axisY["forecolor"].Value);
+                        if (axisY.ContainsKey("minimum"))
+                            y.Minimum = (int)axisY["minimum"].Value;
+                        if (axisY.ContainsKey("maximum"))
+                            y.Maximum = (int)axisY["maximum"].Value;
+                        if (axisY.ContainsKey("interval"))
+                            y.Interval = (int)axisY["interval"].Value;
+                    });
+
+                    a.ChartAreas.Add(tmpChrtArea);
+                }
+
+                foreach (var json_series in (List<JsonStruct>)_value["series"].Value)
+                {
+                    Series tmpSeries = new Series();
+                    foreach (var json_points in (List<JsonStruct>)json_series["points"].Value)
+                    {
+                        DataPoint tmpPoint = new DataPoint();
+                        tmpPoint.AxisLabel = (string)json_points["axislabel"].Value;
+                        List<double> tmpYvalues = new List<double>();
+                        foreach (var json_Yvalue in (List<JsonStruct>)json_points["yvalues"].Value)
+                            tmpYvalues.Add((int)json_Yvalue.Value);
+                        tmpPoint.YValues = tmpYvalues.ToArray();
+                        tmpSeries.Points.Add(tmpPoint);
+                    }
+                    tmpSeries.XValueType = (ChartValueType)Enum.Parse(typeof(ChartValueType), (string)json_series["xvaluetype"].Value);
+                    dummy.Series.Add(tmpSeries);
+                }
+            });
             return dummy;
         }
 
@@ -113,7 +134,7 @@ namespace BasicGrapher
 
         private System.Drawing.Color GetColor(string json_color)
         {
-            return (System.Drawing.Color)Enum.Parse(typeof(System.Drawing.Color), json_color);
+            return System.Drawing.Color.FromName(json_color);
         }
     }
 }
