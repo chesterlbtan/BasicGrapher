a1ab97d7e24951c6be5d474e0fd8b7536b75e77e
chesterlbtan <chesterlbtan@gmail.com>
2017-07-21 22:28:22 +0800

    Enhancement support the whole LabelStyle for AxisX and AxisY

--- BasicGrapher/ChartJSON.cs
+++ BasicGrapher/ChartJSON.cs
@@ -13,6 +13,11 @@ namespace BasicGrapher
         {
             a(obj);
         }
+
+        public static T GetEnum<T>(MyJSON.JsonStruct input)
+        {
+            return (T)Enum.Parse(typeof(T), (string)input.Value);
+        }
     }
     class ChartJSON : MyJSON
     {
@@ -55,37 +60,99 @@ namespace BasicGrapher
                 {
                     ChartArea tmpChrtArea = new ChartArea();
 
-                    tmpChrtArea.AxisX.With(x =>
-                    {
-                        var axisX = json_chrtArea["axisx"];
-                        x.Title = (string)axisX["title"].Value;
-                        if (axisX.ContainsKey("font"))
-                            x.TitleFont = GetFont(axisX["font"]);
-                        if (axisX.ContainsKey("forecolor"))
-                            x.TitleForeColor = GetColor((string)axisX["forecolor"].Value);
-                        if (axisX.ContainsKey("minimum"))
-                            x.Minimum = (int)axisX["minimum"].Value;
-                        if (axisX.ContainsKey("maximum"))
-                            x.Maximum = (int)axisX["maximum"].Value;
-                        if (axisX.ContainsKey("interval"))
-                            x.Interval = (int)axisX["interval"].Value;
-                    });
+                    if (json_chrtArea.ContainsKey("axisx"))
+                        tmpChrtArea.AxisX.With(x =>
+                        {
+                            var axisX = json_chrtArea["axisx"];
+                            x.Title = (string)axisX["title"].Value;
+                            if (axisX.ContainsKey("font"))
+                                x.TitleFont = GetFont(axisX["font"]);
+                            if (axisX.ContainsKey("forecolor"))
+                                x.TitleForeColor = GetColor((string)axisX["forecolor"].Value);
+                            if (axisX.ContainsKey("minimum"))
+                                x.Minimum = (int)axisX["minimum"].Value;
+                            if (axisX.ContainsKey("maximum"))
+                                x.Maximum = (int)axisX["maximum"].Value;
+                            if (axisX.ContainsKey("interval"))
+                                x.Interval = (int)axisX["interval"].Value;
 
-                    tmpChrtArea.AxisY.With(y =>
-                    {
-                        var axisY = json_chrtArea["axisy"];
-                        y.Title = (string)axisY["title"].Value;
-                        if (axisY.ContainsKey("font"))
-                            y.TitleFont = GetFont(axisY["font"]);
-                        if (axisY.ContainsKey("forecolor"))
-                            y.TitleForeColor = GetColor((string)axisY["forecolor"].Value);
-                        if (axisY.ContainsKey("minimum"))
-                            y.Minimum = (int)axisY["minimum"].Value;
-                        if (axisY.ContainsKey("maximum"))
-                            y.Maximum = (int)axisY["maximum"].Value;
-                        if (axisY.ContainsKey("interval"))
-                            y.Interval = (int)axisY["interval"].Value;
-                    });
+                            if (axisX.ContainsKey("labelstyle"))
+                                x.LabelStyle.With(xLabelStyle =>
+                                {
+                                    var json_xLblStyle = axisX["labelstyle"];
+                                    if (json_xLblStyle.ContainsKey("angle"))
+                                        xLabelStyle.Angle = (int)json_xLblStyle["angle"].Value;
+                                    if (json_xLblStyle.ContainsKey("enabled"))
+                                        xLabelStyle.Enabled = Convert.ToBoolean((string)json_xLblStyle["enabled"].Value);
+                                    if (json_xLblStyle.ContainsKey("font"))
+                                        xLabelStyle.Font = GetFont(json_xLblStyle["font"]);
+                                    if (json_xLblStyle.ContainsKey("forecolor"))
+                                        xLabelStyle.ForeColor = GetColor((string)json_xLblStyle["forecolor"].Value);
+                                    if (json_xLblStyle.ContainsKey("format"))
+                                        xLabelStyle.Format = (string)json_xLblStyle["format"].Value;
+                                    if (json_xLblStyle.ContainsKey("interval"))
+                                        xLabelStyle.Interval = (int)json_xLblStyle["interval"].Value;
+                                    if (json_xLblStyle.ContainsKey("intervaloffset"))
+                                        xLabelStyle.IntervalOffset = (int)json_xLblStyle["intervaloffset"].Value;
+                                    if (json_xLblStyle.ContainsKey("intervaloffsettype"))
+                                        xLabelStyle.IntervalOffsetType = Extension.GetEnum<DateTimeIntervalType>(json_xLblStyle["intervaloffsettype"]);
+                                    if (json_xLblStyle.ContainsKey("intervaltype"))
+                                        xLabelStyle.IntervalType = Extension.GetEnum<DateTimeIntervalType>(json_xLblStyle["intervaltype"]);
+                                    if (json_xLblStyle.ContainsKey("isendlabelvisible"))
+                                        xLabelStyle.IsEndLabelVisible = (bool)json_xLblStyle["isendlabelvisible"].Value;
+                                    if (json_xLblStyle.ContainsKey("isstaggered"))
+                                        xLabelStyle.IsStaggered = (bool)json_xLblStyle["isstaggered"].Value;
+                                    if (json_xLblStyle.ContainsKey("truncatedlabels"))
+                                        xLabelStyle.TruncatedLabels = (bool)json_xLblStyle["truncatedlabels"].Value;
+                                });
+                        });
+
+                    if (json_chrtArea.ContainsKey("axisy"))
+                        tmpChrtArea.AxisY.With(y =>
+                        {
+                            var axisY = json_chrtArea["axisy"];
+                            y.Title = (string)axisY["title"].Value;
+                            if (axisY.ContainsKey("font"))
+                                y.TitleFont = GetFont(axisY["font"]);
+                            if (axisY.ContainsKey("forecolor"))
+                                y.TitleForeColor = GetColor((string)axisY["forecolor"].Value);
+                            if (axisY.ContainsKey("minimum"))
+                                y.Minimum = (int)axisY["minimum"].Value;
+                            if (axisY.ContainsKey("maximum"))
+                                y.Maximum = (int)axisY["maximum"].Value;
+                            if (axisY.ContainsKey("interval"))
+                                y.Interval = (int)axisY["interval"].Value;
+
+                            if (axisY.ContainsKey("labelstyle"))
+                                y.LabelStyle.With(yLabelStyle =>
+                                {
+                                    var json_yLblStyle = axisY["labelstyle"];
+                                    if (json_yLblStyle.ContainsKey("angle"))
+                                        yLabelStyle.Angle = (int)json_yLblStyle["angle"].Value;
+                                    if (json_yLblStyle.ContainsKey("enabled"))
+                                        yLabelStyle.Enabled = Convert.ToBoolean((string)json_yLblStyle["enabled"].Value);
+                                    if (json_yLblStyle.ContainsKey("font"))
+                                        yLabelStyle.Font = GetFont(json_yLblStyle["font"]);
+                                    if (json_yLblStyle.ContainsKey("forecolor"))
+                                        yLabelStyle.ForeColor = GetColor((string)json_yLblStyle["forecolor"].Value);
+                                    if (json_yLblStyle.ContainsKey("format"))
+                                        yLabelStyle.Format = (string)json_yLblStyle["format"].Value;
+                                    if (json_yLblStyle.ContainsKey("interval"))
+                                        yLabelStyle.Interval = (int)json_yLblStyle["interval"].Value;
+                                    if (json_yLblStyle.ContainsKey("intervaloffset"))
+                                        yLabelStyle.IntervalOffset = (int)json_yLblStyle["intervaloffset"].Value;
+                                    if (json_yLblStyle.ContainsKey("intervaloffsettype"))
+                                        yLabelStyle.IntervalOffsetType = Extension.GetEnum<DateTimeIntervalType>(json_yLblStyle["intervaloffsettype"]);
+                                    if (json_yLblStyle.ContainsKey("intervaltype"))
+                                        yLabelStyle.IntervalType = Extension.GetEnum<DateTimeIntervalType>(json_yLblStyle["intervaltype"]);
+                                    if (json_yLblStyle.ContainsKey("isendlabelvisible"))
+                                        yLabelStyle.IsEndLabelVisible = (bool)json_yLblStyle["isendlabelvisible"].Value;
+                                    if (json_yLblStyle.ContainsKey("isstaggered"))
+                                        yLabelStyle.IsStaggered = (bool)json_yLblStyle["isstaggered"].Value;
+                                    if (json_yLblStyle.ContainsKey("truncatedlabels"))
+                                        yLabelStyle.TruncatedLabels = (bool)json_yLblStyle["truncatedlabels"].Value;
+                                });
+                        });
 
                     a.ChartAreas.Add(tmpChrtArea);
                 }
